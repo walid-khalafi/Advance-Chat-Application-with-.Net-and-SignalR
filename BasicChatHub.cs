c2c111810cfe020a7eeb68bc21d5db09d04b78d9
Walid Khalafi <walid.khalafi@icloud.com>
2026-05-04 11:29:06 +0330

    Update BasicChatHub.cs

--- BasicChatHub.cs
+++ BasicChatHub.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.SignalR;
-using Microsoft.CodeAnalysis.FlowAnalysis.DataFlow;
 using System.Security.Claims;
 
 namespace SignalRMVC
@@ -9,62 +8,44 @@ namespace SignalRMVC
     public class BasicChatHub : Hub
     {
         private readonly UserManager<IdentityUser> _userManager;
-        HttpContext? _httpContext;
-        string _userId;
-
         public BasicChatHub(UserManager<IdentityUser> userManager)
         {
-            if (_httpContext == null)
-            {
-                _httpContext = Context.GetHttpContext();
-            }
-
-            _userId = _httpContext.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
             _userManager = userManager;
         }
         public override async Task OnConnectedAsync()
         {
-
-            var roles = await GetUserRoles(_userId);
+            var httpContext = Context.GetHttpContext();
+            var userId = httpContext.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var roles = await GetUserRoles(userId);
             // Now you can use the userId as needed
             await base.OnConnectedAsync();
         }
 
         public string GetUserId()
         {
-            return _userId;
-        }
-        public async Task<IdentityUser> GetUserAsync(string userId)
-        {
-            IdentityUser? user = await _userManager.FindByIdAsync(userId);
-            if (user != null)
-            {
-                return user;
-            }
-            return null;
+            var httpContext = Context.GetHttpContext();
+            var userId = httpContext.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            return userId;
         }
 
         public async Task<IList<string>> GetUserRoles(string userId)
         {
-            IdentityUser? user = await _userManager.FindByIdAsync(userId);
-            IList<string> roles = await _userManager.GetRolesAsync(user);
+            var user = await _userManager.FindByIdAsync(userId);
+            var roles = await _userManager.GetRolesAsync(user);
             return roles;
         }
 
         public static List<string> GroupsJoined { get; set; } = new List<string>();
 
         [Authorize]
-        public async Task JoinGroup(string senderId)
+        public async Task JoinGroup(string sender)
         {
-         
-            var role = (await GetUserRoles(_userId)).FirstOrDefault();
+            var user = GetUserId();
+            var role = (await GetUserRoles(user)).FirstOrDefault();
             if (!GroupsJoined.Contains(Context.ConnectionId + ":" + role))
             {
                 GroupsJoined.Add(Context.ConnectionId + ":" + role);
                 //do something else
-
-                var sender = GetUserAsync(senderId);
-
                 await Groups.AddToGroupAsync(Context.ConnectionId, role);
             }
         }
